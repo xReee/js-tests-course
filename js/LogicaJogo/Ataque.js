b02eb9d50fc17ae43a0b8b4601ea42d97801bc3b
xReee <renata.faria.gomes@hotmail.com>
2022-08-16 23:57:02 -0300

    code breaking hoho

--- js/LogicaJogo/Ataque.js
+++ js/LogicaJogo/Ataque.js
@@ -11,7 +11,7 @@ export class Ataque {
             if (armaDefensiva == this.armas[count]) {
                 this.acertouAtaque();
             } else if (armaDefensiva in this.armas) {
-                this.acertouArma();
+                
             }
 
             count++;
